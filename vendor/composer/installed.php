ffe2091966864762be5188cfd5205949a3a2586f
German <gm6353701@gmail.com>
2025-05-09 10:22:41 -0500

    Actualización de archivos

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'proyecto_sena/car-fast',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2ab44ac6362514386a2879f05083abcc9ca61ce8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'proyecto_sena/car-fast' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2ab44ac6362514386a2879f05083abcc9ca61ce8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
